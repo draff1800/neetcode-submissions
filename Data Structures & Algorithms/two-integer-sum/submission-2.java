27a13125cb7ff9578e6595cfe8eaf7cb987d7009
Dylan Rafferty <29754340+draff1800@users.noreply.github.com>
2026-04-28 15:36:07 +0100

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int index1 = 0;
+        int index2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int tempSum = nums[i] + nums[j];
+                if (tempSum == target) {
+                    int[] answer = new int[]{i, j};
+                    Arrays.sort(answer);
+                    return answer;
+                }
+            }
+        }
+        return new int[]{0,0};
+    }
+}
