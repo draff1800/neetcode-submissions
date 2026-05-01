41c19ef95d3d0dc699faafd3bd035b763adc15ae
Dylan Rafferty <29754340+draff1800@users.noreply.github.com>
2026-05-01 18:25:19 +0100

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lowestBuyPrice = prices[0];
+        int highestProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i-1] < lowestBuyPrice) {
+                lowestBuyPrice = prices[i-1];
+            }
+
+            int profit = prices[i] - lowestBuyPrice;
+
+            if (profit > highestProfit) {
+                highestProfit = profit;
+            }
+        }
+
+        return highestProfit;
+    }
+}
